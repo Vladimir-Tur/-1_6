671a4175979f90ee248983c6ae9244e14e14a708
Vladimir-Tur <vovanru2033@mail.ru>
2024-09-13 23:26:13 +0400

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,23 @@
+my_dict = {'Vladimir': 2001, 'Oksana': 1996}
+print(my_dict)
+print(my_dict ['Vladimir'])
+print(my_dict.get ('Sasha'))
+my_dict.update({'Danil': 1993,
+                'Kirimov': 1994 })
+print(my_dict)
+a = my_dict.pop ('Oksana')
+print(a)
+print(my_dict)
+my_set = {1,1,2, 24.65, 'Chack'}
+print(my_set)
+my_set.add ('melon')
+my_set.add (26.78)
+my_set.remove (24.65)
+print(my_set)
+
+
+
+
+
+
+
